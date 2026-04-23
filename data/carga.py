f2b1a980cf7b9b119473e58687d266181043a8ad
johan sebastian henaotilano <henaotilanosebas@gmail.com>
2026-04-23 12:05:25 -0500

    se arreglo el orden y se mejoro el analisis

--- data/carga.py
+++ data/carga.py
@@ -1,14 +1,14 @@
-from numpy import int64
+
 import pandas as pd
 
 def cargar_comercio ():
-    df_comercioComercio=pd.read_csv('datta/row/comercio.csv')
+    df_comercioComercio=pd.read_csv('./row/comercio.csv')
     df_comercioComercio.info()
    
     return df_comercioComercio
 
 def cargar_categoria():
-    df_categoria=pd.read_csv('datta/row/categoria.csv')
+    df_categoria=pd.read_csv('./row/categoria.csv')
     df_categoria.info()
 
-    return
+    return df_categoria
